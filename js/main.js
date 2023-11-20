25f99cf6250dd0e54f69f236ac180084966f8728
P421K <146746331+P421K@users.noreply.github.com>
2023-11-20 20:15:22 +0100

    update js, random colors

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
+//
 const btnSelector = document.querySelectorAll(".color");
 const selectionSelector = document.querySelector(".selectorActual");
+
+//ramdom de colores
+const colors = ["red", "green", "blue", "Yellow"];
+let final = randomFinal();
+
+function randomFinal() {
+  const finalFinal = [...new Array(4)].map((color) => {
+    const random = Math.floor(Math.random() * Math.floor(colors.length));
+    return colors[random];
+  });
+  console.log(finalFinal);
+  return finalFinal;
+}
 
 btnSelector.forEach((btn) => {
   const color = btn.classList[1];
@@ -9,7 +23,7 @@ btnSelector.forEach((btn) => {
 function selectColor(color) {
   console.log(color);
 
-  const div = document.createElement('div');
+  const div = document.createElement("div");
   div.classList.add("itemSeleccionado");
   div.classList.add(color);
 
